0c7308d0705be1fb71e5b0695c77b72bb8e528f4
wpickeral <willpickeral@gmail.com>
2023-04-10 12:07:52 -0400

    exercise one

---
+++ Method.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace method_exercises
+{
+    public class Method
+    {
+        public static void MadLib()
+        {
+            Console.Write("Welcome to Exercise 1\nLet's write a funny story!\nEnter a name: ");
+            var name = Console.ReadLine();
+
+            Console.Write("Enter a verb: ");
+            var verb = Console.ReadLine();
+
+            Console.Write("Enter an animal: ");
+            var animal = Console.ReadLine();
+
+            Console.Write("Enter a food: ");
+            var food = Console.ReadLine();
+
+            Console.Write("Enter an adjective: ");
+            var adjective = Console.ReadLine();
+
+            string story = $"\n{name} went to the pet store to purchase a pet {animal}.\nAfter leaving the pet store, {name} assumed the {animal} was hungry because it was {adjective}, so {name} offered the {animal} some {food}.\nAfter eating all of the {food}, {name} and {animal} walked home to meet the rest of the family.";
+            Console.Write(story);
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace method_exercises;
+
+internal class Program
+{ 
+ public static void Main(string[] args)
+    { 
+        Method.MadLib();
+    }
+}
