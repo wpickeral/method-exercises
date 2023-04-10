dada5b43ce36c3f4625266ae6b4e97cfef24e852
wpickeral <willpickeral@gmail.com>
2023-04-10 12:48:23 -0400

    exercise two

---
+++ MathIsFun.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace method_exercises
+{
+    public class MathIsFun
+    {
+        public static int Add(params int[] items)
+        {
+            int sum = 0;
+            foreach (int item in items)
+            {
+                sum = sum + item;
+            }
+            return sum;
+        }
+
+        public static int Subtract(params int[] items)
+        {
+            int difference = 0;
+            foreach (int item in items)
+            {
+                difference = difference - item;
+            }
+            return difference;
+        }
+
+        public static int Multiply(params int[] items)
+        {
+            int total = 1;
+            foreach (int item in items)
+            {
+                total = item * 1;
+            }
+            return total;
+        }
+
+        public static float Divide(float num1, float num2)
+        {
+            return num1 / num2;
+        }
+
+        public static bool Even(int num)
+        {
+            return num % 2 == 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@ namespace method_exercises;
 internal class Program
 { 
  public static void Main(string[] args)
-    { 
-        Method.MadLib();
+    {
+        // Exercise 1
+        //Method.MadLib();
+
+        // Exercise 2
+        var sum = MathIsFun.Add(2, 3);
+        var difference = MathIsFun.Subtract(2, 3);
+        var multiply = MathIsFun.Multiply(2, 3);
+        var divide = MathIsFun.Divide(2, 5);
+        var isEven = MathIsFun.Even(7);
+
+        Console.WriteLine(sum);
+        Console.WriteLine(difference);
+        Console.WriteLine(multiply);
+        Console.WriteLine(divide);
+        Console.WriteLine(isEven);
     }
 }
